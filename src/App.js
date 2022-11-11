25863adfbf7e38eee26da7224c6bf5e653212504
justinlooong91 <pak_iong_long@brown.edu>
2022-11-10 23:47:50 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,29 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [myArray, setMyArray] = useState([]);
+  const [myPrice, setMyPrice] = useState(0);
+  function addToCart(index, name, price) {
+    setMyArray([...myArray, name]);
+    setMyPrice(myPrice + price);
+  }
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      {bakeryData.map((item, index) => (
+        <BakeryItem
+          key={index}
+          item={item}
+          index={index}
+          addToCart={addToCart}
+        />
       ))}
-
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <li> {myArray}</li>
+
+        <h2>Total: {myPrice}</h2>
       </div>
     </div>
   );
